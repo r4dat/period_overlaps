8e495e79cadefb714d69039f42cbe8ecb990996b
Rob R <RRutherford@khconline.org>
2018-01-03 14:17:18 -0600

    Update DateRange test stub, change test file name to be more descriptive.

---
+++ test_DateRange.py
@@ -0,0 +1,5 @@
+import pytest
+
+#TODO Switch to pytest from Unittest implementation.
+def test_DateRange_creation():
+    assert False
--- test_overlap_check.py
+++ test_overlap_check.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 def test_disjoint():
     assert False
 
