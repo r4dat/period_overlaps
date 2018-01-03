e33badd23b32127d216069144989b1df66f5f6a1
Rob R <RRutherford@khconline.org>
2018-01-03 11:18:18 -0600

    Remove namedtuple, implement basic sanity checks for DateRange init.

--- period_overlap.py
+++ period_overlap.py
@@ -4,7 +4,17 @@
 from datetime import date
 from collections import namedtuple
 
-Date_Range = namedtuple('begin','end')
+class DateRange():
+    def __init__(self,begin,end):
+        _begin_type_check = isinstance(begin,date)
+        _end_type_check = isinstance(end,date)
+        if not (_begin_type_check or _end_type_check):
+            raise TypeError("begin or end is not a datetime.date")
+        if not (begin<=end):
+            raise AttributeError("Start date must be <= end date")
+
+        self.begin = begin
+        self.end = end
 
 a = Date_Range(begin = date(2017,1,1),end = date(2017,2,1))
 b = Date_Range(begin = date(2017,1,15),end = date(2017,5,1))
